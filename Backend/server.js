c02e48692f6dd6461d97662bf946646466446470
JuliusE1402 <juliusejeanye@icloud.com>
2025-07-11 13:45:49 +0200

    Add presentation mode and improve bingo field fetching
    Introduces a 'presentation' mode to the bingo game, updates API endpoints and UI to support fetching and displaying presentation terms, and refines the user interface for managing bingo fields. Also improves initial field fetching logic in the Home component and adds a button to fetch new words in GamePlay.

--- Backend/server.js
+++ Backend/server.js
@@ -350,10 +350,41 @@ const informaticsLectureStrings = [
 ];
 
 const PresentationTermsStrings = [
-	"1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-	"11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
-	"21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
-	"31", "32", "33", "34", "35"
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"10",
+	"11",
+	"12",
+	"13",
+	"14",
+	"15",
+	"16",
+	"17",
+	"18",
+	"19",
+	"20",
+	"21",
+	"22",
+	"23",
+	"24",
+	"25",
+	"26",
+	"27",
+	"28",
+	"29",
+	"30",
+	"31",
+	"32",
+	"33",
+	"34",
+	"35",
 ];
 
 async function initializeDatabase() {
@@ -400,9 +431,7 @@ async function initializeDatabase() {
 			);
 			const docs = PresentationTermsStrings.map((s) => ({ phrase: s }));
 			await PresentationTerms.insertMany(docs);
-			console.log(
-				`${docs.length} Dokumente in PresentationTerms eingefügt.`
-			);
+			console.log(`${docs.length} Dokumente in PresentationTerms eingefügt.`);
 		} else {
 			console.log(
 				"PresentationTerms Collection enthält bereits Daten. Überspringe Befüllung."
